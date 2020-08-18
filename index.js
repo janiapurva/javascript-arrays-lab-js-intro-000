213877ae1ba315fd5f86d954a4ac2cf0b59d8fea
APURVA MAHNEDRABHAI JANI <jani.apurva@yahoo.com>
2020-08-18 23:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyRemoveLastKitten(){
 function destructivelyRemoveFirstKitten(){
   kittens.shift()
 }
-function  appendKitten(kittens){
-  [...kittens,"Broom"]
-  return kittens
+function  appendKitten(){
+  [...kittens,"Broom"];
+  return kittens;
 }
